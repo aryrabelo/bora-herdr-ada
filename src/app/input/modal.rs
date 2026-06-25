4002c6f59c850624f7e595c36f78757cefa4cd51
Ary Rabelo <aryrabelo@gmail.com>
2026-06-25 09:39:23 -0300

    chore: fix cargo fmt formatting

--- src/app/input/modal.rs
+++ src/app/input/modal.rs
@@ -1039,7 +1039,9 @@ pub(crate) fn handle_context_menu_key(
                 let items = menu.items();
                 loop {
                     menu.list.move_prev();
-                    if items.get(menu.list.highlighted) != Some(&crate::app::state::CONTEXT_MENU_SEPARATOR) {
+                    if items.get(menu.list.highlighted)
+                        != Some(&crate::app::state::CONTEXT_MENU_SEPARATOR)
+                    {
                         break;
                     }
                     if menu.list.highlighted == 0 {
@@ -1054,7 +1056,9 @@ pub(crate) fn handle_context_menu_key(
                 let len = items.len();
                 loop {
                     menu.list.move_next(len);
-                    if items.get(menu.list.highlighted) != Some(&crate::app::state::CONTEXT_MENU_SEPARATOR) {
+                    if items.get(menu.list.highlighted)
+                        != Some(&crate::app::state::CONTEXT_MENU_SEPARATOR)
+                    {
                         break;
                     }
                     if menu.list.highlighted >= len.saturating_sub(1) {
